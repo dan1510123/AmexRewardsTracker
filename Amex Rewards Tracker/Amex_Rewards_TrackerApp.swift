e4bef61daad32b98d367d9fce986400028f002bf
Daniel Luo <dyl24@case.edu>
2021-06-11 18:54:19 -0700

    Initial Commit

---
+++ Amex Rewards Tracker/Amex_Rewards_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amex_Rewards_TrackerApp.swift
+//  Amex Rewards Tracker
+//
+//  Created by Daniel Luo on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Amex_Rewards_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
