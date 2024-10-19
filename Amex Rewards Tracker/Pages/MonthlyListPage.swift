a301bca8454973ed87e0eea92e9934c4222884ea
Daniel Luo <dyl24@case.edu>
2024-10-19 14:07:16 -0400

    Updated the leading and trailing buttons of monthly list view

--- Amex Rewards Tracker/Pages/MonthlyListPage.swift
+++ Amex Rewards Tracker/Pages/MonthlyListPage.swift
@@ -20,6 +20,8 @@ struct MonthlyListPage: View {
     
     let buttonWidth: CGFloat = 180
     
+    let monthNames: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    
     init(year: Int, month: Int, viewContext: NSManagedObjectContext) {
         self.year = year
         self.month = month
@@ -45,14 +47,14 @@ struct MonthlyListPage: View {
                 }
             }) {
                 Text("ADMIN MODE")
-                    .font(.system(size: 18, weight: .semibold)) // Updated font
+                    .font(.system(size: 18, weight: .semibold))
                     .foregroundColor(.white)
-                    .padding() // Added padding for a better touch area
+                    .padding()
                     .frame(maxWidth: .infinity)
-                    .background(adminMode ? Color.blue : Color.gray) // Animated color change
-                    .clipShape(Capsule()) // Capsule shape for a softer look
+                    .background(adminMode ? Color.blue : Color.gray)
+                    .clipShape(Capsule())
             }
-            .padding(.horizontal, 40) // Additional padding for spacing
+            .padding(.horizontal, 40)
             .padding(.bottom, 10)
         }
         .background(Color(UIColor.systemBackground))
@@ -60,17 +62,19 @@ struct MonthlyListPage: View {
     
     private func getLeadingButton() -> some View {
         Group {
-            if(adminMode) {
+            if adminMode {
                     EditButton()
                         .frame(width: buttonWidth, alignment: .leading)
             } else {
-                Button(action:  {
-                    year = year + (Int)((month + 10) / 12 - 1)
-                    month = (month + 10) % 12 + 1
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                        Text("Prev")
+                if month != 1 {
+                    Button(action:  {
+                        year = year + (Int)((month + 10) / 12 - 1)
+                        month = (month + 10) % 12 + 1
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                            Text(monthNames[month - 2])
+                        }
                     }
                 }
             }
@@ -81,16 +85,18 @@ struct MonthlyListPage: View {
     
     private func getTrailingButton() -> some View {
         Group {
-            if(adminMode) {
+            if adminMode {
                 NavigationLink("Add Monthly Reward", destination: AddRewardPage(annual: annual, rewardType: "Monthly"))
             } else {
-                Button(action:  {
-                    year = year + (Int)(month / 12)
-                    month = month % 12 + 1
-                }){
-                    HStack {
-                        Text("Next")
-                        Image(systemName: "chevron.right")
+                if month != 12 {
+                    Button(action:  {
+                        year = year + (Int)(month / 12)
+                        month = month + 1
+                    }){
+                        HStack {
+                            Text(monthNames[month])
+                            Image(systemName: "chevron.right")
+                        }
                     }
                 }
             }
@@ -100,11 +106,8 @@ struct MonthlyListPage: View {
     }
     
     private func getMonthYearString() -> String {
-        // Define months array outside the string construction
-        let months: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        
         // Use a separate variable to get the month name
-        let monthName = (month >= 1 && month <= 12) ? months[month - 1] : "Invalid Month"
+        let monthName = (month >= 1 && month <= 12) ? monthNames[month - 1] : "Invalid Month"
         
         return "\(monthName) \(year) Rewards"
     }
