55c4d0a26720408f874bb634f4d31cdec93d63d2
Daniel Luo <dyl24@case.edu>
2024-10-21 13:09:54 -0400

    Updated progress bar with annual fee mark, along with hard-coded fees for now

--- CC Rewards Tracker/Components/ProgressBar.swift
+++ CC Rewards Tracker/Components/ProgressBar.swift
@@ -9,29 +9,44 @@ import SwiftUI
 
 struct ProgressBar: View {
     let barColor: Color
-    var value: Float
+    let progress: Float
+    let goal: Float
     
-    init(value: Float, color: Color) {
+    init(progressPercent: Float, goalPercent: Float, color: Color) {
+        self.progress = progressPercent
+        self.goal = goalPercent
         self.barColor = color
-        self.value = value
     }
     
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
-                Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
+                // Background Bar
+                Rectangle()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                     .opacity(0.3)
                     .foregroundColor(self.barColor.opacity(0.7))
                 
-                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                // Progress Bar
+                Rectangle()
+                    .frame(width: min(CGFloat(self.progress) * geometry.size.width, geometry.size.width), height: geometry.size.height)
                     .foregroundColor(self.barColor)
-            }.cornerRadius(45.0)
+                
+                // Goal Marker
+                let goalPosition = CGFloat(self.goal) * geometry.size.width
+                Rectangle()
+                    .frame(width: 2, height: geometry.size.height)
+                    .foregroundColor(.red)
+                    .offset(x: goalPosition - 1, y: 0)
+            }
+            .cornerRadius(45.0)
         }
+        .frame(height: 20) // Set a height for the progress bar
     }
 }
 
 struct ProgressBar_Previews: PreviewProvider {
     static var previews: some View {
-        Text("")
+        ProgressBar(progressPercent: 0.6, goalPercent: 0.8, color: Color.blue)
     }
 }
